23e433718ab67316aa2bc03a3cc6166d25458da2
MirandaChen <yinglanc@andrew.cmu.edu>
2018-07-09 17:49:38 -0400

    finally a working version

--- parse.sh
+++ parse.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
+# rm -r streams
+# mkdir streams
+
 cd deepmine-alpha-data
+rm list_all.txt
+rm unique.txt
 
 # create a list of files to merge, contains duplicates
-for file in *
+for file in player*
 do
   date=$(echo ${file} |cut -d "-" -f1)
   version=$(echo ${file} |cut -d "-" -f2)
@@ -12,60 +17,23 @@ done
 # remove duplicates and store to a new file
 sort -u list_all.txt > unique.txt
 
-echo "now merge"
 # merge files from the same user with the same stream version
 while read p; do
-  echo $p
-  # ensure orders? cat *.txt >> all.txt?
-  cat $p* > merge_$p.bin
+  # ensure alphabtical order
+  cat $p* > ../streams/merge_$p.bin
 done <unique.txt
 
-# echo *.txt | xargs cat > all.txt
 
-#  For Bash, IIRC, that's alphabetical order. 
-#  If the order is important, you should either name the files appropriately 
-#  (01file.txt, 02file.txt, etc...) or specify each file in the order you want it concatenated.
+cd ..
+mkdir result # a temporary folder
 
-# $ cat file1 file2 file3 file4 file5 file6 > out.txt
-# cat >> ...
+for file in ./streams/recording*
+do
+	./parse -f $file
+done
 
-# nested for loop?
-# 	first one: fix one stream, find "username"
-# 	next: loop through, append to it, cat >>, and delete that file???
+zip -r result.mcpr result
 
-# player_stream_262CPI_06_29_20_16_04-1-2018-07-05-19-39-56-8b5dd8a6-0ece-48f1-ba26-f1326dc5b374
-# player_stream_*_06_29_20_16_04-1-2018-07-05-19-39-56-8b5dd8a6-0ece-48f1-ba26-f1326dc5b374
-
-# Any filenames that match the glob are gathered up and sorted
-# rem parse and zip them one by one
-
-# git clone to download the parse executable?
-# for each src_stream to parse:
-# rem for file in *; do rm "$file"; done
-
-
-# $ filename="somefile.jpg"
-# $ if [[ $filename = *.jpg ]]; then
-# > echo "$filename is a jpeg"
-# > fi
-# somefile.jpg is a jpeg
-
-
-
-# for file in `ls`|cut -d"-" -f1
-# do
-#   cat ${file}-* > ${file}
-# done
-
-
-# 	mkdir result
-# 	./parse -f <src_stream>
-
-# 	zip -r my-folder.zip my-folder -x "*.DS_Store"
-# 	rm -r result
-
-# shopt -s extglob
-#  echo !(*jpg|*bmp)
 
 
 
