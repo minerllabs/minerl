45433c3631dad02c8a11a81864141efb388fad2d
MirandaChen <yinglanc@andrew.cmu.edu>
2018-08-07 12:56:50 -0400

    support reparse if no EOF

--- merging/parse.c
+++ merging/parse.c
@@ -34,6 +34,7 @@ struct stat st = {0};
 
 const int FILE_NUM = 15;
 const int EOF_ENTRY = 13;
+const int META_ENTRY = 1;
 const int BUF_LEN = 500000;
 
 const char* FILES[] =  {
@@ -94,55 +95,41 @@ const char* JSON[]=
 #endif
 
 /* function prototypes */
+void reparse(FILE* input, unsigned int last_valid_seq_num);
+void clean_files();
+void open_append_files(FILE** outputs);
+void close_append_files(FILE** outputs);
+void write_stream_meta_data(int has_EOF, int miss_seq_num);
 unsigned int string_to_unsigned_int(char* buf);
 unsigned int get_entry(char* buf, FILE* stream);
 unsigned int get_sequence_number(char* buf, FILE* stream);
 unsigned int get_time_stamp(char* buf, FILE* stream);
 unsigned int get_len(char* buf, FILE* stream);
-void parse(FILE* input);
+bool parse(FILE* input, unsigned int* last_valid_seq_num_p, unsigned int* end);
 
 
 /* implementation */
-
-void parse(FILE* input)
+// parse: return has_EOF
+bool parse(FILE* input, unsigned int* last_valid_seq_num_p, unsigned int* end)
 { 
     // TODO: error handling, check return of fread value 
     FILE* output; // for overwrite files
     FILE* outputs[FILE_NUM]; // for append files
-    FILE* meta;
-    int meta_len = 0;
+    
     int counter = 0, err_check;
     char buf[BUF_LEN];
     char* backup_buf = NULL;
     int backup_buf_len = 0;
 
-
-    
     unsigned int entry, time_stamp, len, sequence_number;
     unsigned int prev_entry = 0, prev_len = 0;
-    int checked_seq_num = 0;
+    bool checked_seq_num = false;
     bool miss_seq_num = false;
     bool has_EOF = false;
     
     // open all "a" output
-    for (int i = 1; i < FILE_NUM; i++)
-    {
-        if (WRITE_METHODS[i] == APPEND)
-        {
-            outputs[i] = fopen(FILES[i],"w+");
-            if (outputs[i] == NULL)
-            {
-                printf("    error opening file %s. Abort\n", FILES[i]);
-                exit(-1);
-            }
-            else
-            {
-                dbg_printf("successfully open file [%d] %s\n", i, FILES[i]);
-            }
-        }
-    }
+    open_append_files(outputs);
 
-    // to-do: distinguish EOF and error
     while ((err_check= fread(buf, 4, 1, input)) == 1)
     {
         if (has_EOF)
@@ -160,7 +147,7 @@ void parse(FILE* input)
         sequence_number = get_sequence_number(buf, input);
 
         /* check miss_seq_num */ 
-        if (checked_seq_num == 0 && counter != sequence_number)
+        if (!checked_seq_num && counter != sequence_number)
         {
             miss_seq_num = true;
             checked_seq_num = true;
@@ -171,9 +158,8 @@ void parse(FILE* input)
             if (prev_len <= BUF_LEN) printf("data = %s\n",buf);
             else if (backup_buf != NULL) printf("data = %s\n", backup_buf);
             printf("Abort\n");
-            exit(1);
+            exit(-1);
         }
-
 
         /* time_stamp */
         fread(buf, 4, 1, input);
@@ -196,9 +182,9 @@ void parse(FILE* input)
                 backup_buf = malloc(sizeof(len)); 
                 if (backup_buf == NULL)
                 {
-                printf("    failed to allocate new buffer, abort\n");
-                // no file leaks since close rightaway
-                exit(-1);
+                    printf("    failed to allocate new buffer, abort\n");
+                    // no file leaks since close rightaway
+                    exit(-1);
                 }
                 backup_buf_len = len;
             }
@@ -210,7 +196,6 @@ void parse(FILE* input)
             fread(buf, len, 1, input);
             dbg_printf("data = %s\n", buf);
         }
-        
         
         /* open and write output */
         if (0 < entry && entry < FILE_NUM)
@@ -252,19 +237,153 @@ void parse(FILE* input)
             exit(-1);
         }
 
-        /* check has_EOF */ /* question: no entries afterwards? */
-        if (entry == EOF_ENTRY)
-        {
-            has_EOF = true;
-        }
 
+        /* UPDATE */
         dbg_printf("file position: %ld\n\n", ftell(input));
         counter++;
         prev_len = len;
         prev_entry = entry;
+        /* check has_EOF */ 
+        if (entry == EOF_ENTRY)
+        {
+            has_EOF = true;
+        }
+        /* update last_valid_seq_num_p */
+        if (last_valid_seq_num_p != NULL && entry == META_ENTRY)
+        {
+            *last_valid_seq_num_p = sequence_number;
+        }
+        /* check if we hit an endpoint and want to stop */
+        if (end != NULL && sequence_number == *end)
+        {
+            break;
+        }
     }
 
     // close all "a" output
+    close_append_files(outputs);
+
+    // create stream_meta_data
+    write_stream_meta_data(has_EOF, miss_seq_num);
+
+    return has_EOF;
+}
+
+
+/* MAIN */
+int main(int argc, char **argv)
+{   
+    int opt;
+    size_t err_check;
+    FILE* input;
+    unsigned int last_valid_seq_num;
+    bool has_EOF;
+
+   
+    const char* src_stream = NULL;
+
+    // get src_stream
+    while ((opt = getopt(argc, argv, "f:")) != -1){
+        switch (opt) {
+            case 'f':
+                src_stream = optarg;
+                break;
+        }
+    }
+
+    if (src_stream == NULL)
+    {
+        printf("    No src_stream fount. Program abort. Please run ./parse -f <filename>\n");
+        exit(-1);
+    }
+
+    // display info
+    printf("running %s...\n", src_stream);
+
+    // open input file
+    input = fopen(src_stream,"r");
+    if (input == NULL)
+    {
+        printf("    Error opening the source stream. Abort.\n");
+        exit(-1);
+    }
+
+    // the main parse function
+    has_EOF = parse(input, &last_valid_seq_num, NULL);
+    
+    // close input file
+    err_check = fclose(input);
+    if (err_check != 0)
+    {
+        printf("error closing the source stream. \n");
+    }
+
+    // if no EOF, set end point, reparse again
+    if (!has_EOF)
+    {
+        reparse(input, last_valid_seq_num);
+    }
+
+    // finish and return
+    printf("finish parsing the source stream.\n");
+    return 0;
+}
+
+/* HELPER FUNCTIONS */
+void reparse(FILE* input, unsigned int last_valid_seq_num)
+{
+    printf("WARNING: no EOF detected. Reparse the stream to keep the valid part. \n");
+    clean_files();
+    unsigned int* end = malloc(sizeof(unsigned int));
+    *end = last_valid_seq_num;
+    // input is read only so safe to reuse
+    parse(input, NULL, end);
+    free(end);  
+}
+
+void clean_files()
+{
+    FILE* stream;
+    for (int i = 1; i < FILE_NUM; i++)
+    {
+        stream = fopen(FILES[i],"w+");
+        if (stream == NULL) 
+        {
+            printf("error cleaning file %s. Unable to reparse. Abort.\n", FILES[i]);
+            exit(-1);
+        }
+        if (fclose(stream) != 0){
+            printf("error cleaning file %s. Unable to reparse. Abort.\n", FILES[i]);
+            exit(-1);
+        }
+    }
+}
+
+void open_append_files(FILE** outputs)
+{
+    int err_check;
+    // open all "a" output
+    for (int i = 1; i < FILE_NUM; i++)
+    {
+        if (WRITE_METHODS[i] == APPEND)
+        {
+            outputs[i] = fopen(FILES[i],"w+");
+            if (outputs[i] == NULL)
+            {
+                printf("    error opening file %s. Abort\n", FILES[i]);
+                exit(-1);
+            }
+            else
+            {
+                dbg_printf("successfully open file [%d] %s\n", i, FILES[i]);
+            }
+        }
+    }
+}
+
+void close_append_files(FILE** outputs)
+{
+    int err_check;
     for (int i = 1; i < FILE_NUM; i++)
     {
         if (WRITE_METHODS[i]== APPEND)
@@ -281,8 +400,13 @@ void parse(FILE* input)
             }
         }
     }
+}
 
-    // create stream_meta_data
+void write_stream_meta_data(int has_EOF, int miss_seq_num)
+{
+    FILE* meta;
+    int meta_len = 0;
+    char buf[BUF_LEN];
     meta = fopen("./result/stream_meta_data.json", "w+");
     if (meta == NULL) printf("error creating stream_meta_data.json\n");
     // four cases 
@@ -309,57 +433,6 @@ void parse(FILE* input)
     fwrite(buf, meta_len, 1, meta);
     fclose(meta);
 }
-
-
-/* MAIN */
-int main(int argc, char **argv)
-{   
-    int opt;
-    size_t err_check;
-    FILE* input;
-    // const char* src_stream = default_src_stream;
-    const char* src_stream = NULL;
-
-    // get src_stream
-    while ((opt = getopt(argc, argv, "f:")) != -1){
-        switch (opt) {
-            case 'f':
-                src_stream = optarg;
-                break;
-        }
-    }
-
-    if (src_stream == NULL)
-    {
-        printf("    No src_stream fount. Program abort. Please run ./parse -f <filename>\n");
-        exit(-1);
-    }
-    // display info
-    printf("running %s...\n", src_stream);
-
-    // open input file
-    input = fopen(src_stream,"r");
-    if (input == NULL)
-    {
-        printf("    Error opening the source stream. Abort.\n");
-        exit(-1);
-    }
-
-    // the main parse function
-    parse(input);
-    
-    // close input file
-    err_check = fclose(input);
-    if (err_check != 0)
-    {
-        printf("error closing the source stream. \n");
-    }
-    // finish and return
-    printf("finish parsing the source stream.\n");
-    return 0;
-}
-
-/* HELPER FUNCTIONS */
 
 /* convert string to unsigned int*/
 unsigned int string_to_unsigned_int(char* buf)
