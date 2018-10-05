3054f2faa492aa0a0e1dd628cab9d9383f3fcbc1
Brandon Houghton <bhoughton@cmu.edu>
2018-10-05 15:08:16 -0400

    merged.

--- merge.sh
+++ merge.sh
@@ -10,11 +10,11 @@
 
 home=$(pwd)
 working_dir=$home/output
-results_dir=$working_dir/merged
+results_dir=$working_dir/merged_old
 
 
 # input folder 
-path=$working_dir/downloaded
+path=$working_dir/downloaded_old
 echo "Loading streams from $path"
 if [ ! -d $path ]; then
   echo "Could not find parsed files. Did you not download from s3?"
