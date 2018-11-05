2d0b745a2774c655489f1d8c87ed0bff602e4b98
Brandon Houghton <bhoughton@cmu.edu>
2018-11-05 16:01:15 -0500

    Updated autorender script

--- auto_render.sh
+++ auto_render.sh
@@ -2,10 +2,10 @@
 
 cd ~/cmu-rl/herobraine_parse
 # warning: the path is hardcoded in download.py
-/home/cmr-rl/herobraine_parse/python3 ./download2.py
+/usr/bin/python3.6 ./download2.py
 ./merge.sh
-home/cmr-rl/herobraine_parse/python3 ./render.py
-home/cmr-rl/herobraine_parse/python3 ./generate.py
+/usr/bin/python3.6 ./render.py
+/usr/bin/python3.6 ./generate.py
 
 
 
