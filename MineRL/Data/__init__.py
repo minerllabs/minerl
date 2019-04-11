56d560051b4f233b68b43a5f3b4860670758eb3d
Brandon Houghton <bhoughton@cmu.edu>
2019-04-11 15:12:10 -0700

    Added draft for framework - Readme for competition

---
+++ MineRL/Data/__init__.py
@@ -0,0 +1,18 @@
+def init():
+    pass
+
+
+def reset():
+    pass
+
+
+def filter(fn):
+    pass
+
+
+def get():
+    pass
+
+
+def batch():
+    pass
---
+++ MineRL/MalmoEnv/__init__.py
@@ -0,0 +1,14 @@
+def init():
+    pass
+
+
+def reset():
+    pass
+
+
+def step():
+    pass
+
+
+def get_action_space():
+    pass
