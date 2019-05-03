ea5c35d06c003ca942b68b6228d4870a4fc44619
Brandon Houghton <bhoughton@cmu.edu>
2019-05-03 13:23:43 -0400

    Add environment variables to dataset download script

--- minerl/data/downloader/__init__.py
+++ minerl/data/downloader/__init__.py
@@ -1,10 +1,29 @@
+import os
 import requests
 import tqdm
 
 
-def download(directory, resolution='low', texturePack=0):
+def download(directory: os.path, resolution: str = 'low', texture_pack: int = 0, update_enviroment_variables=True) -> None:
+    """
+    Downloads MineRLv0 to specified directory. If directory is None downloads to $MINERL_DATA_ROOT. Throws error if both
+    are not defined.
+    :param directory:
+    :type directory:
+    :param resolution:
+    :type resolution:
+    :param texture_pack:
+    :type texture_pack:
+    """
+    if directory is None:
+        if 'MINERL_DATA_ROOT' in os.environ and len(os.environ['MINERL_DATA_ROOT']) > 0:
+            directory = os.environ['MINERL_DATA_ROOT']
+        else:
+            raise ValueError("Provided directory is None and $MINERL_DATA_ROOT is not defined")
+    elif update_enviroment_variables:
+        os.environ['MINERL_DATA_ROOT'] = directory
 
-    url = '/'.join(["https://router.sneakywines.me/minerl/data_texture_1_low_res.tar.gz", resolution, texturePack])
+    # TODO pull JSON defining dataset URLS from webserver instead of hard-coding
+    url = "https://router.sneakywines.me/minerl/data_texture_{}_{}_res.tar.gz".format(texture_pack, resolution)
 
     response = requests.get(url, stream=True)
 
