35f1ddbd8ae1fe9294f26df97c0304b7169c44cd
Brandon Houghton <bhoughton@cmu.edu>
2019-05-22 15:49:59 -0600

    Added NavigateExtreme environments and fixed navigationDenseFixedMap Added environment MineRLNavigateExtreme-v0 Added environment MineRLNavigateExtremeDense-v0 Changed compass randomization to reflect human experiment parameters Added fixed_surface placement type for navigation decorator

--- minerl/env/__init__.py
+++ minerl/env/__init__.py
@@ -61,55 +61,28 @@ register(
 #######################
 ## NAVIGATE
 #######################
-register(
-    id='MineRLNavigateDense-v0',
-    entry_point='minerl.env:MineRLEnv',
-    kwargs={
-        'xml': os.path.join(missions_dir, 'navigationDense.xml'),
-        'observation_space': gym.spaces.Dict({
-            'pov': gym.spaces.Box(low=0, high=255, shape=(64,64,3), dtype=np.uint8),
-            'inventory': gym.spaces.Dict({
-                'dirt': gym.spaces.Box(low=0, high=2304, shape=(1,), dtype=np.int)
-            }),
-            'compassAngle': gym.spaces.Box(low=-180.0, high=180.0, shape=(1,), dtype=np.float32)
-        }),
-        'action_space': gym.spaces.Dict({
-            "forward": gym.spaces.Discrete(2), 
-            "back": gym.spaces.Discrete(2), 
-            "left": gym.spaces.Discrete(2), 
-            "right": gym.spaces.Discrete(2), 
-            "jump": gym.spaces.Discrete(2), 
-            "sneak": gym.spaces.Discrete(2), 
-            "sprint": gym.spaces.Discrete(2), 
-            "attack" : gym.spaces.Discrete(2), 
-            "camera": gym.spaces.Box(low=-180, high=180, shape=(2,), dtype=np.float32), # Pitch, Yaw
-            "placeblock": spaces.Enum('none', 'dirt')
-        })
-    },
-    max_episode_steps=6000,
-)
 
 register(
-    id='MineRLNavigateDenseFixed-v0',
+    id='MineRLNavigate-v0',
     entry_point='minerl.env:MineRLEnv',
     kwargs={
-        'xml': os.path.join(missions_dir, 'navigationDenseFixedMap.xml'),
+        'xml': os.path.join(missions_dir, 'navigation.xml'),
         'observation_space': gym.spaces.Dict({
-            'pov': gym.spaces.Box(low=0, high=255, shape=(64,64,3), dtype=np.uint8),
+            'pov': gym.spaces.Box(low=0, high=255, shape=(64, 64, 3), dtype=np.uint8),
             'inventory': gym.spaces.Dict({
                 'dirt': gym.spaces.Box(low=0, high=2304, shape=(1,), dtype=np.int)
             }),
             'compassAngle': gym.spaces.Box(low=-180.0, high=180.0, shape=(1,), dtype=np.float32)
         }),
         'action_space': gym.spaces.Dict({
-            "forward": gym.spaces.Discrete(2), 
-            "back": gym.spaces.Discrete(2), 
-            "left": gym.spaces.Discrete(2), 
-            "right": gym.spaces.Discrete(2), 
-            "jump": gym.spaces.Discrete(2), 
-            "sneak": gym.spaces.Discrete(2), 
-            "sprint": gym.spaces.Discrete(2), 
-            "attack" : gym.spaces.Discrete(2), 
+            "forward": gym.spaces.Discrete(2),
+            "back": gym.spaces.Discrete(2),
+            "left": gym.spaces.Discrete(2),
+            "right": gym.spaces.Discrete(2),
+            "jump": gym.spaces.Discrete(2),
+            "sneak": gym.spaces.Discrete(2),
+            "sprint": gym.spaces.Discrete(2),
+            "attack" : gym.spaces.Discrete(2),
             "camera": gym.spaces.Box(low=-180, high=180, shape=(2,), dtype=np.float32),
             "placeblock": spaces.Enum('none', 'dirt')
         }),
@@ -118,10 +91,100 @@ register(
 )
 
 register(
-    id='MineRLNavigate-v0',
+    id='MineRLNavigateDense-v0',
     entry_point='minerl.env:MineRLEnv',
     kwargs={
-        'xml': os.path.join(missions_dir, 'navigation.xml'),
+        'xml': os.path.join(missions_dir, 'navigationDense.xml'),
+        'observation_space': gym.spaces.Dict({
+            'pov': gym.spaces.Box(low=0, high=255, shape=(64, 64, 3), dtype=np.uint8),
+            'inventory': gym.spaces.Dict({
+                'dirt': gym.spaces.Box(low=0, high=2304, shape=(1,), dtype=np.int)
+            }),
+            'compassAngle': gym.spaces.Box(low=-180.0, high=180.0, shape=(1,), dtype=np.float32)
+        }),
+        'action_space': gym.spaces.Dict({
+            "forward": gym.spaces.Discrete(2), 
+            "back": gym.spaces.Discrete(2), 
+            "left": gym.spaces.Discrete(2), 
+            "right": gym.spaces.Discrete(2), 
+            "jump": gym.spaces.Discrete(2), 
+            "sneak": gym.spaces.Discrete(2), 
+            "sprint": gym.spaces.Discrete(2), 
+            "attack" : gym.spaces.Discrete(2), 
+            "camera": gym.spaces.Box(low=-180, high=180, shape=(2,), dtype=np.float32),  # Pitch, Yaw
+            "placeblock": spaces.Enum('none', 'dirt')
+        })
+    },
+    max_episode_steps=6000,
+)
+
+
+register(
+    id='MineRLNavigateExtreme-v0',
+    entry_point='minerl.env:MineRLEnv',
+    kwargs={
+        'xml': os.path.join(missions_dir, 'navigationExtreme.xml'),
+        'observation_space': gym.spaces.Dict({
+            'pov': gym.spaces.Box(low=0, high=255, shape=(64, 64, 3), dtype=np.uint8),
+            'inventory': gym.spaces.Dict({
+                'dirt': gym.spaces.Box(low=0, high=2304, shape=(1,), dtype=np.int)
+            }),
+            'compassAngle': gym.spaces.Box(low=-180.0, high=180.0, shape=(1,), dtype=np.float32)
+        }),
+        'action_space': gym.spaces.Dict({
+            "forward": gym.spaces.Discrete(2),
+            "back": gym.spaces.Discrete(2),
+            "left": gym.spaces.Discrete(2),
+            "right": gym.spaces.Discrete(2),
+            "jump": gym.spaces.Discrete(2),
+            "sneak": gym.spaces.Discrete(2),
+            "sprint": gym.spaces.Discrete(2),
+            "attack" : gym.spaces.Discrete(2),
+            "camera": gym.spaces.Box(low=-180, high=180, shape=(2,), dtype=np.float32),
+            "placeblock": spaces.Enum('none', 'dirt')
+        }),
+    },
+    max_episode_steps=6000,
+)
+
+register(
+    id='MineRLNavigateExtremeDense-v0',
+    entry_point='minerl.env:MineRLEnv',
+    kwargs={
+        'xml': os.path.join(missions_dir, 'navigationExtremeDense.xml'),
+        'observation_space': gym.spaces.Dict({
+            'pov': gym.spaces.Box(low=0, high=255, shape=(64, 64, 3), dtype=np.uint8),
+            'inventory': gym.spaces.Dict({
+                'dirt': gym.spaces.Box(low=0, high=2304, shape=(1,), dtype=np.int)
+            }),
+            'compassAngle': gym.spaces.Box(low=-180.0, high=180.0, shape=(1,), dtype=np.float32)
+        }),
+        'action_space': gym.spaces.Dict({
+            "forward": gym.spaces.Discrete(2),
+            "back": gym.spaces.Discrete(2),
+            "left": gym.spaces.Discrete(2),
+            "right": gym.spaces.Discrete(2),
+            "jump": gym.spaces.Discrete(2),
+            "sneak": gym.spaces.Discrete(2),
+            "sprint": gym.spaces.Discrete(2),
+            "attack" : gym.spaces.Discrete(2),
+            "camera": gym.spaces.Box(low=-180, high=180, shape=(2,), dtype=np.float32),  # Pitch, Yaw
+            "placeblock": spaces.Enum('none', 'dirt')
+        })
+    },
+    max_episode_steps=6000,
+)
+
+
+#######################
+#        DEBUG        #
+#######################
+
+register(
+    id='MineRLNavigateDenseFixed-v0',
+    entry_point='minerl.env:MineRLEnv',
+    kwargs={
+        'xml': os.path.join(missions_dir, 'navigationDenseFixedMap.xml'),
         'observation_space': gym.spaces.Dict({
             'pov': gym.spaces.Box(low=0, high=255, shape=(64,64,3), dtype=np.uint8),
             'inventory': gym.spaces.Dict({
