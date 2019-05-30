fc1640509b688d5478128b89e29e85f5b04b6a96
Ricky Houghton <ricky.houghton@gmail.com>
2019-05-30 12:42:45 -0400

    Added support for multiple years - only goes to 2020 - hopefully the model for processing data is improved by 2020 : )

--- download2.py
+++ download2.py
@@ -6,7 +6,7 @@ import os
 
 s3 = boto3.resource('s3')
 bucket = s3.Bucket('pizza-party')
-bucket_prefix="2018/"
+bucket_prefixes = ['2018','2019','2020']
 parent_dir = "output"
 target_dir = os.path.join(parent_dir, "downloaded_new")
 
@@ -16,14 +16,16 @@ if not os.path.exists(target_dir):
     os.makedirs(target_dir)
 print("Downloading to {}".format(target_dir))
 
-for obj in tqdm.tqdm(bucket.objects.filter(Prefix=bucket_prefix)):
-    try:
-        key = obj.key
-        filename = key.split("/")[-1]
-        if not os.path.isfile(os.path.join(target_dir, filename)):
-            bucket.download_file(obj.key, os.path.join(target_dir, filename))
-    except botocore.exceptions.ClientError as e:
-        if e.response['Error']['Code'] == "404":
-            print("The object does not exist.")
-        else:
-            raise
+for bucket_prefix in bucket_prefixes:
+    print ("Year: ",bucket_prefix)
+    for obj in tqdm.tqdm(bucket.objects.filter(Prefix=bucket_prefix)):
+        try:
+            key = obj.key
+            filename = key.split("/")[-1]
+            if not os.path.isfile(os.path.join(target_dir, filename)):
+                bucket.download_file(obj.key, os.path.join(target_dir, filename))
+        except botocore.exceptions.ClientError as e:
+            if e.response['Error']['Code'] == "404":
+                print("The object does not exist.")
+            else:
+                raise
