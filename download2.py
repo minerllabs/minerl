6e24115f3a2b7649d6f7b278d9340348a966fdba
Ricky Houghton <ricky.houghton@gmail.com>
2019-06-02 19:07:30 -0400

    Added counter which totals the number of objs and the number of new objects

--- download2.py
+++ download2.py
@@ -16,16 +16,22 @@ if not os.path.exists(target_dir):
     os.makedirs(target_dir)
 print("Downloading to {}".format(target_dir))
 
+obj_count = 0
+new_objs = 0
 for bucket_prefix in bucket_prefixes:
     print ("Year: ",bucket_prefix)
     for obj in tqdm.tqdm(bucket.objects.filter(Prefix=bucket_prefix)):
+
         try:
             key = obj.key
             filename = key.split("/")[-1]
-            if not os.path.isfile(os.path.join(target_dir, filename)):
+        if os.path.isfile(os.path.join(render[1], 'metaData.json')):        
                 bucket.download_file(obj.key, os.path.join(target_dir, filename))
+                new_objs += 1
         except botocore.exceptions.ClientError as e:
             if e.response['Error']['Code'] == "404":
                 print("The object does not exist.")
             else:
                 raise
+        obj_count += 1
+print ("Total Files: ",obj_count, "New Files",new_objs)
