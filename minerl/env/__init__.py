e679a405a7091ca477ec67b5c49362b0181642aa
NAKATA Keisuke <keisuke.nakata.919@gmail.com>
2019-06-24 12:54:32 +0900

    sort action_space key

--- minerl/env/__init__.py
+++ minerl/env/__init__.py
@@ -428,10 +428,10 @@ register(
             "sneak": spaces.Discrete(2),
             "sprint": spaces.Discrete(2),
             "attack": spaces.Discrete(2),
-            "equip": spaces.Enum('none', 'wooden_axe', 'wooden_pickaxe', 'stone_axe', 'stone_pickaxe',
-                                 'iron_pickaxe'),
             "camera": spaces.Box(low=-180, high=180, shape=(2,), dtype=np.float32),  # Pitch, Yaw
             "place": spaces.Enum('none', 'dirt', 'stone', 'cobblestone', 'crafting_table', 'furnace', 'torch'),
+            "equip": spaces.Enum('none', 'wooden_axe', 'wooden_pickaxe', 'stone_axe', 'stone_pickaxe',
+                                 'iron_pickaxe'),
             "craft": spaces.Enum('none', 'torch', 'stick', 'planks', 'crafting_table'),
             "nearbyCraft": spaces.Enum('none', 'wooden_axe', 'wooden_pickaxe', 'stone_axe', 'stone_pickaxe',
                                        'iron_pickaxe', 'furnace'),
