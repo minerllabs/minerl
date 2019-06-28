4881915bfd96e6cf99a4c99f624c09d708da10fe
Brandon Houghton <bhoughton@cmu.edu>
2019-06-28 13:49:18 -0400

    Add simple test, move broken tests to excluded dir

---
+++ tests/basic_import_test.py
@@ -0,0 +1,28 @@
+# Basic package test
+import gym
+import minerl
+
+
+#import minerl.env.bootstrap
+#minerl.env.bootstrap._check_port_avail = lambda _,__: True
+
+NUM_EPISODES=5
+
+def main():
+    """
+    Tests importing of gym envs
+    """
+    env = gym.make('MineRLNavigate-v0')
+    env = gym.make('MineRLNavigateDense-v0')
+    env = gym.make('MineRLNavigateExtreme-v0')
+    env = gym.make('MineRLNavigateExtremeDense-v0')
+    env = gym.make('MineRLObtainIron-v0')
+    env = gym.make('MineRLObtainIronDense-v0')
+    env = gym.make('MineRLObtainDiamond-v0')
+    env = gym.make('MineRLObtainDiamondDense-v0')
+
+    print("Test complete.")
+
+
+if __name__ == "__main__":
+    main()
