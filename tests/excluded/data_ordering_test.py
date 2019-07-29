88489172739e08d1d85cc5110f7b2695c39dbd54
Brandon Houghton <bhoughton@cmu.edu>
2019-07-29 13:38:18 -0400

    Data pipeline fixes + integration tests (#167)
    * Add data downloading and caching to travis build
    
    * Remove extra tab char
    
    * Adjust indent
    
    * Debug
    
    * Cache directories, only one env entry
    
    * Ensure cache is not in temp fs
    
    * Added treading to tarfile extraction
    
    * Test data ordering on all version of python
    
    * Add parallel tests
    
    * Added debug logger to test script
    
    * Added debug logger to test script
    
    * Try beta cache
    
    * Update download script to support downloading single experiments
    
    * Fix travis typo
    
    * Don't re-download experiments if folder exists
    
    * Don't re-download experiments if folder exists
    
    * Use competition environment for testing
    
    * Adjust print statements to please travis
    
    * Reduce length of data sampling t
    
    * Reduce length of data sampling by breaking
    
    * Indent - typo
    
    * test live cache solution
    
    * Test using spaces arg str
    
    * Use ordered dict for load_data_pyfunc
    
    * Small typos
    
    * Install logger to see why 3.5 build is hanging
    
    * Move travis-breaking test to excluded
    Disable download of experiment

---
+++ tests/excluded/data_ordering_test.py
@@ -0,0 +1,77 @@
+import time
+
+import minerl
+import itertools
+import gym
+import sys
+import tqdm
+import numpy as np
+import logging
+
+logging.getLogger().setLevel(logging.DEBUG)
+logging.basicConfig()
+
+
+# Helper functions
+def _check_shape(num_samples, sample_shape, obs):
+    if isinstance(obs, list):
+        assert (len(obs)) == num_samples
+    elif isinstance(obs, np.ndarray):
+        assert (obs.shape[0] == num_samples)
+        for i in range(len(obs.shape) - 1):
+            assert (obs.shape[i + 1] == sample_shape[i])
+    else:
+        assert False, "unsupported data type"
+
+
+def _check_space(key, space, observation, correct_len):
+    if isinstance(space, minerl.spaces.Dict):
+        for k, s in space.spaces.items():
+            _check_space(k, s, observation[key], correct_len)
+    elif isinstance(space, minerl.spaces.MultiDiscrete):
+        # print("MultiDiscrete")
+        # print(space.shape)
+        # print(observation[key])
+        _check_shape(correct_len, space.shape, observation[key])
+    elif isinstance(space, minerl.spaces.Box):
+        # print("Box")
+        # print(space.shape)
+        # print(observation[key])
+        _check_shape(correct_len, space.shape, observation[key])
+    elif isinstance(space, minerl.spaces.Discrete):
+        # print("Discrete")
+        # print(space.shape)
+        # print(observation[key])
+        _check_shape(correct_len, space.shape, observation[key])
+    elif isinstance(space, minerl.spaces.Enum):
+        # print("Enum")
+        # print(space.shape)
+        # print(observation[key])
+        _check_shape(correct_len, space.shape, observation[key])
+    else:
+        assert False, "Unsupported dict type"
+
+
+def test_data(environment='MineRLObtainDiamond-v0'):
+    for _ in range(20):
+        d = minerl.data.make(environment, num_workers=1)
+
+        # Iterate through batches of data
+        for obs, act, rew, nObs, done in d.sarsd_iter(num_epochs=1, max_sequence_len=2):
+            correct_len = len(rew)
+            for key, space in d.observation_space.spaces.items():
+                _check_space(key, space, obs, correct_len)
+
+            for key, space in d.action_space.spaces.items():
+                _check_space(key, space, act, correct_len)
+
+            break
+
+    return True
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        rate = test_data(sys.argv[1])
+    else:
+        rate = test_data()
