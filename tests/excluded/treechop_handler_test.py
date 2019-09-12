35ba6533d0fef22e1a16255d7751defeb408e6b6
Brandon Houghton <bhoughton@cmu.edu>
2019-09-12 17:31:02 -0400

    Update reward handlers, improve unit tests Added reward for possesing item to ensure 64 reward is achieved in treechop Added reward for possesing item to MineRLObtainDiamond-v0 to ensure collection of diamonds is rewarded Added mapping of log to log2 and vice versa for reward handling to prevent double reward

---
+++ tests/excluded/treechop_handler_test.py
@@ -0,0 +1,66 @@
+# Simple env test.
+import json
+import select
+import time
+import logging
+
+import gym
+import matplotlib.pyplot as plt
+import minerl
+import numpy as np
+from minerl.env.core import MineRLEnv
+
+import coloredlogs
+coloredlogs.install(logging.DEBUG)
+
+
+#import minerl.env.bootstrap
+#minerl.env.bootstrap._check_port_avail = lambda _,__: True
+
+def gen_obtain_debug_actions(env):
+    actions = []
+
+    def act(**kwargs):
+        action = env.action_space.no_op()
+        for key, value in kwargs.items():
+            action[key] = value
+        actions.append(action)
+
+    [act(attack=1) for _ in range(40)]
+
+    act(camera=np.array([45.0, 0.0], dtype=np.float32))
+
+    [act(attack=1) for _ in range(40)]
+
+    [act(forward=1) for _ in range(10)]
+
+    return actions
+
+def main():
+    """
+    Tests running a simple environment.
+    """
+    env = gym.make('MineRLTreechopDebug-v0')
+    
+    env.seed(17)
+    obs = env.reset()
+    done = False
+    netr = 0
+    for action in gen_obtain_debug_actions(env):
+        obs, reward, done, info = env.step(action)
+        time.sleep(0.1)
+        env.render()
+        if reward != 0:
+            print(reward)
+        if done:
+            print('done!')
+            break
+            
+
+
+
+
+    print("Demo complete.")
+
+if __name__ == "__main__":
+    main()
