9575da741b80ab782211810a235e46250dabc80b
Brandon Houghton <bhoughton@cmu.edu>
2019-09-13 18:22:47 -0400

    Brandon/reward fixes (#243)
    * Update reward handlers, improve unit tests
    Added reward for possesing item to ensure 64 reward
    is achieved in treechop
    Added reward for possesing item to MineRLObtainDiamond-v0 to ensure
    collection of diamonds is rewarded
    Added mapping of log to log2 and vice versa for reward handling to prevent
    double reward
    
    * Update debugEnv to reflect competition environment
    
    * Added dense version of obtain test

--- minerl/env/__init__.py
+++ minerl/env/__init__.py
@@ -511,3 +511,28 @@ register(
     },
     max_episode_steps=2000,
 )
+
+register(
+    id='MineRLObtainTestDense-v0',
+    entry_point='minerl.env:MineRLEnv',
+    kwargs={
+        'xml': os.path.join(missions_dir, 'obtainDebugDense.xml'),
+        'observation_space': obtain_observation_space,
+        'action_space':  spaces.Dict({
+            "forward": spaces.Discrete(2),
+            "back": spaces.Discrete(2),
+            "left": spaces.Discrete(2),
+            "right": spaces.Discrete(2),
+            "jump": spaces.Discrete(2),
+            "sneak": spaces.Discrete(2),
+            "sprint": spaces.Discrete(2),
+            "attack": spaces.Discrete(2),
+            "camera": spaces.Box(low=-180, high=180, shape=(2,), dtype=np.float32),  # Pitch, Yaw
+            "place": spaces.Enum('none', 'dirt', 'log', 'log2', 'stone', 'cobblestone', 'crafting_table', 'furnace', 'torch', 'diamond_ore'),
+            "equip": spaces.Enum('none', 'air', 'wooden_axe', 'wooden_pickaxe', 'stone_axe', 'stone_pickaxe', 'iron_axe', 'iron_pickaxe'),
+            "craft": spaces.Enum('none', 'torch', 'stick', 'planks', 'crafting_table'),
+            "nearbyCraft": spaces.Enum('none', 'wooden_axe', 'wooden_pickaxe', 'stone_axe', 'stone_pickaxe', 'iron_axe', 'iron_pickaxe', 'furnace'),
+            "nearbySmelt": spaces.Enum('none', 'iron_ingot', 'coal')})
+    },
+    max_episode_steps=2000,
+)
