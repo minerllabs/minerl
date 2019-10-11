d6a190d5e00e6c5720f7e1d01c9e45b68cdb4527
William Guss <carlostnb@gmail.com>
2019-10-11 11:52:17 -0400

    Upgrading the version.

--- setup.py
+++ setup.py
@@ -86,7 +86,7 @@ data_files += package_files('minerl/env/Malmo')
 
 setuptools.setup(
       name='minerl',
-      version='0.2.3',
+      version='0.2.5',
       description='MineRL environment and data loader for reinforcement learning from human demonstration in Minecraft',
       long_description=markdown,
       long_description_content_type="text/markdown",
