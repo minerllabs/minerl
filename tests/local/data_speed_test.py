ae9632f8acf36cbf37f81294735a52858f6bc5e8
MineRL Labs <minerl@andrew.cmu.edu>
2019-10-15 21:38:05 -0400

    Shorten the number of samples for data speed-test

--- tests/local/data_speed_test.py
+++ tests/local/data_speed_test.py
@@ -13,8 +13,9 @@ def time_data(environment='MineRLObtainDiamond-v0'):
 
     # Iterate through batches of data
     counter = tqdm.tqdm()
-    for obs, act, rew, nObs, done in d.sarsd_iter(num_epochs=1, max_sequence_len=128):
-        counter.update(len(rew))
+    for _ in range(1000)
+        for obs, act, rew, nObs, done in d.sarsd_iter(num_epochs=1, max_sequence_len=128):
+            counter.update(len(rew))
 
     return counter.n / counter.last_print_t if counter.last_print_n > 0 else 0
 
