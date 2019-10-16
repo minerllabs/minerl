89b82a17657fa3a132fe90fc3a791bc646e0daa0
MineRL Labs <minerl@andrew.cmu.edu>
2019-10-15 22:01:20 -0400

    Make the test slightly longer ...

--- tests/local/data_speed_test.py
+++ tests/local/data_speed_test.py
@@ -13,7 +13,7 @@ def time_data(environment='MineRLObtainDiamond-v0'):
 
     # Iterate through batches of data
     counter = tqdm.tqdm()
-    for obs, act, rew, nObs, done in itertools.islice(d.sarsd_iter(num_epochs=1, max_sequence_len=128), 100):
+    for obs, act, rew, nObs, done in itertools.islice(d.sarsd_iter(num_epochs=1, max_sequence_len=128), 1000):
         counter.update(len(rew))
 
     return counter.n / counter.last_print_t if counter.last_print_n > 0 else 0
