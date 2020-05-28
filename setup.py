5a5d8191e9763f2b69c93896a4663e1752d61b72
Brandon Houghton <bhoughton@cmu.edu>
2020-05-27 20:49:06 -0400

    Add wraped env test for testing action space

--- setup.py
+++ setup.py
@@ -8,19 +8,21 @@ import subprocess
 
 from distutils.command.build import build
 
-merge_src =  os.path.join(os.path.dirname(__file__),"minerl_data", "pipeline", "parser")
-assets =  os.path.join(os.path.dirname(__file__), "minerl_data", "assets")
+merge_src = os.path.join(os.path.dirname(__file__), "minerl_data", "pipeline", "parser")
+assets = os.path.join(os.path.dirname(__file__), "minerl_data", "assets")
+
+
 class Build(build):
     """Customized setuptools build command - builds protos on build."""
+
     def run(self):
         protoc_command = ["make"]
         if subprocess.call(protoc_command, cwd=os.path.join(
-            merge_src
+                merge_src
         )) != 0:
             sys.exit(-1)
         print("sup")
         super().run()
-
 
 
 def package_files(directory):
@@ -29,9 +31,8 @@ def package_files(directory):
 
         # if not ("Malmo/Malmo" in path): print(path)
         if (
-            not ".git" in path 
+                not ".git" in path
         ):
-            
             paths.append((path, [os.path.join(path, f) for f in filenames if not isdir(f) and (
                 not f == "parse"
             )]))
@@ -44,7 +45,7 @@ setup(
     version='0.0.1',
     packages=setuptools.find_packages(),
     author="William Guss, Brandon Houghton",
-        cmdclass={
+    cmdclass={
         'build': Build,
     },
     data_files=package_files(merge_src),
