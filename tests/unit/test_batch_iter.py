5cb7639a643c71b23ce503e4ad23347993890ba1
MadcowD <carlostnb@gmail.com>
2020-06-09 23:37:40 -0700

    New data pipeline.

---
+++ tests/unit/test_batch_iter.py
@@ -0,0 +1,19 @@
+import minerl
+import os
+import coloredlogs
+import logging
+import numpy as np
+import tqdm
+def test_batch_iter():
+    os.environ['MINERL_DATA_ROOT'] = os.path.expanduser('~/adsd')
+    dat = minerl.data.make('MineRLTreechopVectorObf-v0')
+    
+    act_vectors = []
+    for asd in tqdm.tqdm(dat.batch_iter(16, 32, 1, preload_buffer_size=20)):
+        pass
+
+    
+
+if __name__ == '__main__':
+    # coloredlogs.install(logging.DEBUG)
+    test_batch_iter()
