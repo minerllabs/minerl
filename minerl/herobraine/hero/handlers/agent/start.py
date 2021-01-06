41c04737eab404042c911f57c840ee204bdbfa8d
Adrien Ecoffet <adrien.ecoffet@gmail.com>
2021-01-05 16:59:46 -0800

    Support giving the agent a breaking speed multiplier (#439)

--- minerl/herobraine/hero/handlers/agent/start.py
+++ minerl/herobraine/hero/handlers/agent/start.py
@@ -79,6 +79,19 @@ class SimpleInventoryAgentStart(InventoryAgentStart):
         })
 
 
+class AgentStartBreakSpeedMultiplier(Handler):
+    def to_string(self) -> str:
+        return f"agent_start_break_speed_multiplier({self.multiplier})"
+
+    def xml_template(self) -> str:
+        return str(
+            """<BreakSpeedMultiplier>{{multiplier}}</BreakSpeedMultiplier>"""
+        )
+
+    def __init__(self, multiplier=1.0):
+        self.multiplier = multiplier
+
+
 class AgentStartPlacement(Handler):
     def to_string(self) -> str:
         return f"agent_start_placement({self.x}, {self.y}, {self.z}, {self.yaw}, {self.pitch})"
