91efd17a54dd6ea3343a9d2ceb6930bb37f56d8d
Peter Zhokhov <peterz@openai.com>
2021-01-20 18:03:59 -0800

    .

--- play.sh
+++ play.sh
@@ -2,12 +2,11 @@
 
 function upload {
     while true; do
-        bbb sync $HOME/minerl_recordings az://oaiagidata/data/datasets/minerl_recorder/v2/
+        az storage copy -s ~/minerl_recordings/ -d https://oaiagidata.blob.core.windows.net/data/datasets/minerl_recorder/v2/ --recursive --only-show-errors
         sleep 10
     done
 }
 
-pip install boostedblob
 cd $(dirname $0)
 cd minerl/Malmo/Minecraft
 upload &
