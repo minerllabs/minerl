b47edffb8d1c278674d91bd107f4adb192ba9d1d
Peter Zhokhov <peterz@openai.com>
2021-01-20 18:06:22 -0800

    revert to use bbb

--- play.sh
+++ play.sh
@@ -2,7 +2,8 @@
 
 function upload {
     while true; do
-        az storage copy -s ~/minerl_recordings/ -d https://oaiagidata.blob.core.windows.net/data/datasets/minerl_recorder/v2/ --recursive --only-show-errors
+        bbb sync $HOME/minerl_recordings az://oaiagidata/data/datasets/minerl_recorder/v2/
+        # az storage copy -s ~/minerl_recordings/ -d https://oaiagidata.blob.core.windows.net/data/datasets/minerl_recorder/v2/ --recursive --only-show-errors
         sleep 10
     done
 }
