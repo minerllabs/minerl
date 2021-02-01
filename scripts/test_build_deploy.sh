c177afa23f360dfff906865a54dfe6181df98510
Brandon Houghton <bhoughton@cmu.edu>
2021-02-01 14:18:51 -0800

    Move to azure for storage

--- scripts/test_build_deploy.sh
+++ scripts/test_build_deploy.sh
@@ -17,7 +17,8 @@ mkdir -p $MINERL_DATA_ROOT
 
 # First, we run the tests in the repo
 pip install -e .
-gsutil -m rsync  -r $GS_MINERL_DATA $MINERL_DATA_ROOT
+az storage copy -s $AZ_MINERL_DATA -d $MINERL_DATA_ROOT --recursive --subscription sci
+
 pytest .
 pip uninstall -y minerl
 
@@ -38,6 +39,6 @@ cd ..
 python -c "import minerl; import gym, logging; logging.basicConfig(level=logging.DEBUG); env=gym.make('minerl:MineRLTreechop-v0', is_fault_tolerant=False); env.reset(); env.close()"
 cd $cur_dir
 # Finally, if this is not a cron build, we deploy the wheel
-if [ "$BUILDKITE_SOURCE" != "schedule" ]; then 
-    gsutil cp -a public-read dist/* $GS_UPLOAD_LOCATION/$BUILDKITE_BRANCH/${MINERL_VERSION_NUMBER}-${DATE_SHORT}-${BUILDKITE_COMMIT:0:7}/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/
+if [ "$BUILDKITE_SOURCE" != "schedule" ]; then
+    az storage copy --subscription sci -s dist/* -d $AZ_UPLOAD_LOCATION/$BUILDKITE_BRANCH/${MINERL_VERSION_NUMBER}-${DATE_SHORT}-${BUILDKITE_COMMIT:0:7}/$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)/
 fi
