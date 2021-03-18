55dabfaf662f4cae33f53379b96ddad734d69119
Brandon Houghton <brandon@openai.com>
2021-03-18 13:10:05 -0700

    Update test_build_deploy.sh

--- scripts/test_build_deploy.sh
+++ scripts/test_build_deploy.sh
@@ -19,9 +19,7 @@ mkdir -p $MINERL_DATA_ROOT
 pip install -e .
 #az storage copy -s $AZ_MINERL_DATA -d $MINERL_DATA_ROOT --recursive --subscription sci
 
-
 pytest . -n 2
-
 pip uninstall -y minerl
 
 pip list
