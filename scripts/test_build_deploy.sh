5d18fcba437f07face7787e66e27330dfd3fd325
Brandon Houghton <bhoughton@cmu.edu>
2021-05-10 15:47:13 -0700

    Update test script to address linting issues

--- scripts/test_build_deploy.sh
+++ scripts/test_build_deploy.sh
@@ -5,7 +5,8 @@ set -ex
 export PATH=$JAVA_HOME/bin:$PATH
 # env variables controlling the build version and location in GCS
 export MINERL_VERSION_NUMBER=0.3.7
-export DATE_SHORT=$(date +%Y%m%d-%H%M)
+temp="$(date +%Y%m%d-%H%M)""
+export DATE_SHORT=$temp
 export MINERL_BUILD_VERSION=${MINERL_VERSION_NUMBER}+openai.git.${BUILDKITE_COMMIT:0:7}.date.${DATE_SHORT}
 
 # Adding a single head directly to the init.sh to avoid zombificiaiton from xvfb-run.
@@ -13,7 +14,7 @@ Xvfb :0 -screen 0 1024x768x24 &
 
 #!/bin/bash
 set -ex
-mkdir -p $MINERL_DATA_ROOT
+mkdir -p "$MINERL_DATA_ROOT"
 
 # First, we run the tests in the repo
 pip install -e .
