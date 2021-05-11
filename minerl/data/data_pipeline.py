02266ef08807b61360b1c558d3fbb26d0dc4d46e
Cody Wild <codywild@berkeley.edu>
2021-05-11 11:23:02 -0700

    Fix Pep8 violations

--- minerl/data/data_pipeline.py
+++ minerl/data/data_pipeline.py
@@ -387,10 +387,13 @@ class DataPipeline:
             batch_size (int): The batch size.
             seq_len (int): The size of sequences to produce.
             num_epochs (int, optional): The number of epochs to iterate over the data. Defaults to -1.
-            preload_buffer_size (int, optional): Increase to IMPROVE PERFORMANCE. The data iterator uses a queue to prevent blocking, the queue size is the number of trajectories to load into the buffer. Adjust based on memory constraints. Defaults to 32.
+            preload_buffer_size (int, optional): Increase to IMPROVE PERFORMANCE. The data iterator uses a queue to
+                prevent blocking, the queue size is the number of trajectories to load into the buffer.
+                Adjust based on memory constraints. Defaults to 32.
             seed (int, optional): [int]. NOT IMPLEMENTED Defaults to None.
             include_metadata (bool, optional): Include metadata on the source trajectory. Defaults to False.
-            include_monitor_data (bool, optional): Include monitor data (info dict) on the source trajectory. Defaults to False
+            include_monitor_data (bool, optional): Include monitor data (info dict)
+                on the source trajectory. Defaults to False
 
         Returns:
             Generator: A generator that yields (sarsd) batches
@@ -428,7 +431,8 @@ class DataPipeline:
                     yield yield_dict
 
             # Careful with the ordering here
-            jobs = [(f, -1, None, include_monitor_data, include_metadata) for f in self._get_all_valid_recordings(self.data_dir)]
+            jobs = [(f, -1, None, include_monitor_data, include_metadata)
+                    for f in self._get_all_valid_recordings(self.data_dir)]
             np.random.shuffle(jobs)
             for job in jobs:
                 input_queue.put(job)
@@ -526,6 +530,7 @@ class DataPipeline:
         """
         raise DeprecationWarning(
             "The `DataPipeline.sarsd_iter` method is deprecated! Please use DataPipeline.batch_iter().")
+
 
 def loader_func(input_queue, output_queue):
     while True:
