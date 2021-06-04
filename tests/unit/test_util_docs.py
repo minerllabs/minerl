9d72d18b5599c6f5429bd2ad076ce330950b3f07
Steven H. Wang <wang.steven.h@gmail.com>
2021-06-04 16:26:05 -0700

    Basic basalt docs

--- tests/unit/test_util_docs.py
+++ tests/unit/test_util_docs.py
@@ -4,4 +4,4 @@ from minerl.herobraine import envs
 
 def test_docs_smoke():
     for env_spec in envs.ENV_SPECS:
-        docs.print_actions_for_id(env_spec)
+        docs.print_env_spec_sphinx(env_spec)
