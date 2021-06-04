f9cb993f4150bdfd45a19702de3f85f6939e8519
Cody Wild <codywild@berkeley.edu>
2021-06-04 11:30:24 -0700

    One more pep8 issue

--- tests/test_dataset_version.py
+++ tests/test_dataset_version.py
@@ -2,6 +2,8 @@ from minerl.data.version import get_version_violations
 import os
 
 # Assumes current state where allowed versions are (3, 4)
+
+
 def test_dataset_version():
     violations = get_version_violations(3)
     assert violations is None
