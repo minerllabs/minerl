cfb017d52bac0bf66f479339b7cc2e4d577721a5
Steven H. Wang <wang.steven.h@gmail.com>
2021-07-08 12:49:57 -0700

    setup.py: Remove unused imports

--- setup.py
+++ setup.py
@@ -2,14 +2,11 @@
 # Author: William H. Guss, Brandon Houghton
 
 import os
-import sys
-from os.path import isdir
 
 import subprocess
 import pathlib
 import setuptools
 from setuptools import Command
-from setuptools.command.develop import develop
 from setuptools.command.install import install
 from setuptools.command.install_lib import install_lib
 from distutils.command.build import build
