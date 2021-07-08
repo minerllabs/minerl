452618d6a037a83abc045ae56120337908187445
Brandon Houghton <bhoughton@cmu.edu>
2021-07-08 12:32:56 -0700

    Fix damage source observation to work for all damage

--- minerl/Malmo/Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/ObservationFromDamageImplementation.java
+++ minerl/Malmo/Minecraft/src/main/java/com/microsoft/Malmo/MissionHandlers/ObservationFromDamageImplementation.java
@@ -8,10 +8,13 @@ import com.microsoft.Malmo.MissionHandlerInterfaces.IObservationProducer;
 import com.microsoft.Malmo.Schemas.MissionInit;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityLivingBase;
 import net.minecraft.item.ItemStack;
 import net.minecraft.util.DamageSource;
 import net.minecraft.util.math.Vec3d;
 import net.minecraftforge.event.entity.living.LivingDeathEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 
@@ -23,6 +26,9 @@ import static java.lang.Math.*;
 public class ObservationFromDamageImplementation extends HandlerBase implements IObservationProducer
 {
     private DamageSource damageSource;
+    private float damageAmount;
+    private EntityLivingBase entity;
+    private boolean hasDied;
 
 
 	@Override
@@ -34,32 +40,61 @@ public class ObservationFromDamageImplementation extends HandlerBase implements 
     @SubscribeEvent()
     public void onClientTick(LivingDeathEvent event)
     {
-        // Use the client tick to ensure we regularly update our state (from the client thread)
-        this.damageSource = event.getSource();
+        if (event.getEntityLiving().equals(Minecraft.getMinecraft().player)) {
+            if (this.damageSource != null) {
+                System.out.println("Warning overwriting damage source - entity has died!");
+            }
+            this.damageSource = event.getSource();
+            this.entity = event.getEntityLiving();
+            this.hasDied = true;
+        }
+    }
+    
+    @SubscribeEvent()
+    public void onDamage(LivingHurtEvent event)
+    {
+        if (event.getEntityLiving().equals(Minecraft.getMinecraft().player)) {
+            if (this.damageSource != null) {
+                System.out.println("Warning skipped damage event - multiple damage events in one tick!");
+            }
+            this.damageSource = event.getSource();
+            this.damageAmount = event.getAmount();
+            this.entity = event.getEntityLiving();
+            System.out.println(this.damageAmount + " of damage from " + this.damageSource.getDamageType() + " for entity " + this.entity);
+        }
     }
 
 	@Override
     public void writeObservationsToJSON(JsonObject json, MissionInit missionInit)
     {
         EntityPlayerSP player = Minecraft.getMinecraft().player;
-        JsonObject death_json = new JsonObject();
+        JsonObject damage_json = new JsonObject();
         json.addProperty("is_dead", player.isDead);
+        json.addProperty("livingDeathEventFired", this.hasDied);
+        this.hasDied = false;
+        
+        if (this.damageAmount != 0 && this.damageSource != null)
+            System.out.println(this.damageAmount + " of damage from " + this.damageSource.getDamageType());
+
+        if (this.damageAmount != 0) {
+            damage_json.addProperty("damage_amount", this.damageAmount);
+        }
 
         if (this.damageSource != null) {
-            death_json.addProperty("damage_source", this.damageSource.getDamageType());
-            death_json.addProperty("hunger_damage", this.damageSource.getHungerDamage());
-            death_json.addProperty("is_damage_absolute", this.damageSource.isDamageAbsolute());
-            death_json.addProperty("is_fire_damage", this.damageSource.isFireDamage());
-            death_json.addProperty("is_magic_damage", this.damageSource.isMagicDamage());
-            death_json.addProperty("is_difficulty_scaled", this.damageSource.isDifficultyScaled());
-            death_json.addProperty("is_explosion", this.damageSource.isExplosion());
-            death_json.addProperty("is_projectile", this.damageSource.isProjectile());
-            death_json.addProperty("is_unblockable", this.damageSource.isUnblockable());
-            death_json.addProperty("death_message", this.damageSource.getDeathMessage(player).getUnformattedText());
+            damage_json.addProperty("damage_source", this.damageSource.getDamageType());
+            damage_json.addProperty("hunger_damage", this.damageSource.getHungerDamage());
+            damage_json.addProperty("is_damage_absolute", this.damageSource.isDamageAbsolute());
+            damage_json.addProperty("is_fire_damage", this.damageSource.isFireDamage());
+            damage_json.addProperty("is_magic_damage", this.damageSource.isMagicDamage());
+            damage_json.addProperty("is_difficulty_scaled", this.damageSource.isDifficultyScaled());
+            damage_json.addProperty("is_explosion", this.damageSource.isExplosion());
+            damage_json.addProperty("is_projectile", this.damageSource.isProjectile());
+            damage_json.addProperty("is_unblockable", this.damageSource.isUnblockable());
+            damage_json.addProperty("death_message", this.damageSource.getDeathMessage(player).getUnformattedText());
 
             if (this.damageSource.getEntity() != null) {
-                death_json.addProperty("damage_entity", this.damageSource.getEntity().getName());
-                death_json.addProperty("damage_entity_id", this.damageSource.getEntity().getEntityId());
+                damage_json.addProperty("damage_entity", this.damageSource.getEntity().getName());
+                damage_json.addProperty("damage_entity_id", this.damageSource.getEntity().getEntityId());
                 JsonArray entity_equipment = new JsonArray();
                 // TODO do we need to mark the equipment slot of this armor?
                 for (ItemStack item : this.damageSource.getEntity().getEquipmentAndArmor()) {
@@ -68,18 +103,18 @@ public class ObservationFromDamageImplementation extends HandlerBase implements 
                         entity_equipment.add(jitem);
                     }
                 }
-                death_json.add("damage_entity_equipment", entity_equipment);
+                damage_json.add("damage_entity_equipment", entity_equipment);
             } if (this.damageSource.getDamageLocation() != null) {
-                death_json.addProperty("damage_location", this.damageSource.getDamageLocation().toString());
-                death_json.addProperty("damage_distance", this.damageSource.getDamageLocation().distanceTo(player.getPositionVector()));
+                damage_json.addProperty("damage_location", this.damageSource.getDamageLocation().toString());
+                damage_json.addProperty("damage_distance", this.damageSource.getDamageLocation().distanceTo(player.getPositionVector()));
                 Vec3d attack_vec = this.damageSource.getDamageLocation().subtract(player.getPositionVector());
-                death_json.addProperty("damage_pitch", asin(attack_vec.yCoord));
-                death_json.addProperty("damage_yaw", atan2(attack_vec.xCoord, attack_vec.zCoord));
+                damage_json.addProperty("damage_pitch", asin(attack_vec.yCoord));
+                damage_json.addProperty("damage_yaw", atan2(attack_vec.xCoord, attack_vec.zCoord));
             }
             this.damageSource = null;
         }
 
-        json.add("damage_source", death_json);
+        json.add("damage_source", damage_json);
     }
 
 }
