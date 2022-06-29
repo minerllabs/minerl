2b261caa83d9ef8b963c15e5e29c2933cc3c8d01
neverix <nev@neverix.io>
2022-06-29 21:01:46 +0300

    Make VPT work out of the box

--- minerl/herobraine/env_specs/human_controls.py
+++ minerl/herobraine/env_specs/human_controls.py
@@ -39,10 +39,10 @@ class HumanControlEnvSpec(EnvSpec, ABC):
 
 
     def __init__(self, name, *args,
-            resolution=(1280, 720),
-            guiscale_range=[2, 2],
+            resolution=(640, 360),
+            guiscale_range=[1, 1],
             gamma_range=[2.0, 2.0],
-            fov_range=[130.0,130.0],
+            fov_range=[70.0, 70.0],
             cursor_size_range=[16, 16],
             **kwargs):
 
