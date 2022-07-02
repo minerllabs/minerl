8c00ec51cc1c65fdc8cbec509c29e14be7f5b092
trigaten <sanderschulhoff@gmail.com>
2022-07-02 01:14:09 -0400

    docs

--- minerl/utils/documentation.py
+++ minerl/utils/documentation.py
@@ -30,7 +30,6 @@ def print_env_spec_sphinx(env_spec: EnvSpec) -> None:
     print(f"{env_name}")
     print("_" * len(env_name))
     print(env.get_docstring())
-
     if hasattr(env, "inventory"):
         print("..................")
         print("Starting Inventory")
@@ -40,19 +39,17 @@ def print_env_spec_sphinx(env_spec: EnvSpec) -> None:
             item_id = stack["type"]
             starting_inv_canonical[item_id] = stack["quantity"]
         print(_format_dict(starting_inv_canonical))
-
     if hasattr(env, "max_episode_steps"):
         print("..................")
         print("Max Episode Steps")
         print("..................")
         
         print(f":code:`{env.max_episode_steps}`")
-
     print(".....")
     print("Usage")
     print(".....")
-
     usage_str = f'''.. code-block:: python
+    
         env = gym.make("{env.name}")
     '''
     print(usage_str)
