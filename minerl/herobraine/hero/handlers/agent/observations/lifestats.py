d2a69c93f57ee6c07026ed72487bf4eea772f78d
xieleo5 <55373943+xieleo5@users.noreply.github.com>
2022-12-13 17:37:52 -0500

    fix life status not reading from info problem (#664)
    * fix life status not reading from info problem
    
    * add from_hero and unpack one layer from lifestats

--- minerl/herobraine/hero/handlers/agent/observations/lifestats.py
+++ minerl/herobraine/hero/handlers/agent/observations/lifestats.py
@@ -40,11 +40,15 @@ class LifeStatsObservation(KeymapTranslationHandler):
     def __init__(self, hero_keys, univ_keys, space, default_if_missing=None):
         self.hero_keys = hero_keys
         self.univ_keys = univ_keys
-        super().__init__(hero_keys=hero_keys, univ_keys=['life_stats'] + univ_keys, space=space,
-                         default_if_missing=default_if_missing)
+        super().__init__(hero_keys=hero_keys, univ_keys=['life_stats'] + univ_keys,
+                         space=space, default_if_missing=default_if_missing)
 
     def xml_template(self) -> str:
         return str("""<ObservationFromFullStats/>""")
+
+    def from_hero(self, hero_dict):
+        hero_dict = hero_dict['life_stats']
+        return super().from_hero(hero_dict)
 
 
 class _IsAliveObservation(LifeStatsObservation):
